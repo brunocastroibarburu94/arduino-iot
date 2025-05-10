1d0c9c9e387d84a1edd2865f022266795250eef0
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2025-05-10 19:03:21 +0100

    Building Arduino project in C++, using PlatformIO

--- ArduinoMega/HelloWorld/hello_world/hello_world.ino
+++ ArduinoMega/HelloWorld/hello_world/hello_world.ino
@@ -1,30 +1,38 @@
-const int  analogPin = A0;// Analog input pin A0
-int sensorValue=0; // Variable to store the value
-const int outputPin = 7; // A
 
 // Connections:
 // Moisture Sensor: GND to GND/ VCC to 5V, AOUT to Analog Input Port i.e., A0
 // Water Level Sensor: Connecting the water sensor reader, + to 5V, - to GND, S to Analog Input Port i.e., A0
+// Relay is controlled through Pin 7
+
+// ANALOG PINS
+#define WATER_LEVEL_SENSOR_PIN A0
+#define GROUND_MOISTURE_SENSOR_PIN A1
+
+// DIGITAL PINS
+#define RELAY_PIN 7
 
 // Data:
 // The relay when powered off is Closed
 // When powered on if IN is 1 is Closed
 // When powered on if IN is 0 is Open
-const int CLOSE_RELAY=1;
-const int OPEN_RELAY=0;
+#define CLOSE_RELAY 1
+#define OPEN_RELAY 0
 
 // Moisture Sensor: On Air is 469, 430 very dr soil, Freshly wet soil is 200 
-const float max__moisture_dry=469.0;
-const float hth__dry_soil=400.0;
-const float lth__wet_soil=250.0;
-const float min__moisture_wet=200.0;
+#define max__moisture_dry 469
+#define hth__dry_soil 400
+#define lth__wet_soil 250
+#define min__moisture_wet 200
 
 // Water Level sensor:
 // Reading of 660 is fully submerged, if taken out of the water readingg oes to 340
-const float max__water_level=660.0; 
-const float hth__water_level=600.0; 
-const float lth__water_level=400.0; 
-const float min__water_level=340.0; // When wet a layer of water accumulates at the bottom, if fully dry it can reach 0. 
+#define max__water_level 660 
+#define hth__water_level 600 
+#define lth__water_level 400 
+#define min__water_level 340 // When wet a layer of water accumulates at the bottom, if fully dry it can reach 0. 
+
+int water_level=0; // Variable to store the value
+const int outputPin = 7; // A
 
 int out_value=0;
 int state = 0;
---
+++ PlatformIO/platformio-hello-world/Readme.md
@@ -0,0 +1,16 @@
+# PlatformIO projects
+
+### Useful shell commands
+
+```bash
+# On a Command Prompt shell
+
+# Build project
+C:\Users\bruno\.platformio\penv\Scripts\platformio.exe run
+
+# Upload project to board
+C:\Users\bruno\.platformio\penv\Scripts\platformio.exe run --target upload --environment megaADK 
+
+# Monitor Serial communication
+C:\Users\bruno\.platformio\penv\Scripts\platformio.exe device monitor --environment megaADK 
+```

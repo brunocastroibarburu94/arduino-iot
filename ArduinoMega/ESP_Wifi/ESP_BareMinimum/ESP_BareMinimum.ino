a62b33a5abce44209e08f539775fe60b7c10dd73
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2025-05-20 20:29:09 +0100

    ESP tutorial instructions

---
+++ ArduinoMega/ESP_Wifi/ESP_BareMinimum/ESP_BareMinimum.ino
@@ -0,0 +1,23 @@
+// Follow: https://www.instructables.com/Getting-Started-With-the-ESP8266-ESP-01/
+
+// Connections: 
+// ESP    -> Arduino UNO
+
+// TX     -> Pin 1
+// RX     -> Pin 0
+// CH_PD  -> 3.3V
+// RST    -> Unwired
+// VCC    -> 3.3V
+// GND    -> GND
+// GPIO2  -> Unwired
+// GPIO0  -> Unwired
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
